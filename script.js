510bfd91c7349697f59e2b4f28f7af56482bc96d
ltbob <cpt.bob@gmail.com>
2024-06-28 13:42:48 -0600

    Modifed JS with suggestions from littleAndian

--- script.js
+++ script.js
@@ -2,39 +2,53 @@ const itemBox = document.getElementById("item-box");
 const quantityBox = document.getElementById("quantity-box");
 const listContainer = document.getElementById("list-container");
 
-let userName;
-
-function login() {
+// get userName from login for use in other functions
+const getUserName = () => {
   const email = document.getElementById("email").value;
-  if (email === "cpt.bob@gmail.com") {
-    userName = "CptBob";
-  } else {
-    userName = null;
-  }
-  return userName;
-}
+  return email === "cpt.bob@gmail.com" ? "CptBob" : null;
+};
 
-function handleLogin(userName) {
+// function to run handlelogin function onclick and allow the logout link to be seen
+function login() {
+  handleLogin();
   const loginBox = document.getElementById("login-box");
   const login = document.getElementById("login-link");
   const logout = document.getElementById("logout-link");
+  // const userName = getUserName();
+
+  loginBox.classList.toggle("show");
+  logout.classList.toggle("show");
+  login.classList.toggle("hide");
+}
+
+// function to set username to null, change the user back to the login link and hide the logout link
+function logout() {
+  const login = document.getElementById("login-link");
+  const logout = document.getElementById("logout-link");
   const user = document.getElementById("user-name");
+  user.innerHTML = "";
+  logout.classList.toggle("show");
+  login.classList.toggle("hide");
+  user.classList.toggle("show");
+}
+
+// function to set the username and show it in the HTML
+function handleLogin() {
+  const user = document.getElementById("user-name");
+  const userName = getUserName();
 
   if (userName) {
     console.log(userName);
     user.innerHTML = `${userName}`;
-    loginBox.classList.toggle("show");
-    logout.classList.toggle("show");
-    login.classList.toggle("hide");
     user.classList.toggle("show");
   } else {
     alert("Unkown User");
   }
 }
 
+// event listeners for login/logout functions
 document.querySelector(".js-login-button").addEventListener("click", () => {
-  let currentUser = login();
-  handleLogin(currentUser);
+  login();
 });
 
 document.querySelector(".js-login-link").addEventListener("click", () => {
@@ -44,64 +58,66 @@ document.querySelector(".js-login-link").addEventListener("click", () => {
   email.focus();
 });
 
-document
-  .querySelector(".js-logout-link")
-  .addEventListener("click", (userName) => {
-    const login = document.getElementById("login-link");
-    const logout = document.getElementById("logout-link");
-    const user = document.getElementById("user-name");
-    user.innerHTML = "";
-    userName = "";
-    logout.classList.toggle("show");
-    login.classList.toggle("hide");
-    user.classList.toggle("show");
-  });
+document.querySelector(".js-logout-link").addEventListener("click", () => {
+  logout();
+});
 
+// function to add the items to the list
 function addTask() {
+  const userName = getUserName();
+
   if (itemBox.value === "" || quantityBox.value === "") {
     alert("Please enter the missing item or quantity");
     return;
   }
-  // create li for future list items
-  let li = document.createElement("li");
-  listContainer.appendChild(li);
+  {
+    // create li for future list items
+    const li = document.createElement("li");
 
-  // create item for the list and add
-  let item = document.createElement("span");
-  item.classList.add("item");
-  item.innerHTML = itemBox.value;
-  li.appendChild(item);
+    // create item for the list and add
+    const item = document.createElement("span");
+    item.classList.add("item");
+    item.innerHTML = itemBox.value;
+    li.appendChild(item);
 
-  // create quantity for the list and add
-  let quantity = document.createElement("span");
-  quantity.classList.add("quantity");
-  quantity.innerHTML = quantityBox.value;
-  li.appendChild(quantity);
+    // create quantity for the list and add
+    const quantity = document.createElement("span");
+    quantity.classList.add("quantity");
+    quantity.innerHTML = quantityBox.value;
+    li.appendChild(quantity);
 
-  // create user for the list and add from login
-  let user = document.createElement("span");
-  user.classList.add("user");
-  user.innerHTML = `${userName}`;
-  li.appendChild(user);
+    // create user for the list and add from login
+    const user = document.createElement("span");
+    user.classList.add("user");
+    user.innerHTML = `${userName}`;
+    li.appendChild(user);
 
-  // create close "X" to remove the selected li from the list
-  let close = document.createElement("span");
-  close.classList.add("close");
-  close.innerHTML = "&#215;";
-  li.appendChild(close);
+    // create close "X" to remove the selected li from the list
+    close = document.createElement("span");
+    close.classList.add("close");
+    close.innerHTML = "&#215;";
+    li.appendChild(close);
+
+    // update all items
+    listContainer.appendChild(li);
+  }
+  // reset the inputs, return focus to the first box and run savedata function
+  itemBox.value = "";
+  quantityBox.value = "";
+  itemBox.focus();
+  saveData();
 }
-itemBox.value = "";
-quantityBox.value = "";
-itemBox.focus();
-saveData();
 
+// event listener to add item to the list
 document.querySelector(".js-add-button").addEventListener("click", () => {
   addTask();
 });
 
+// event listener to strikethrough items that are in the cart and add the ability to remove them
 listContainer.addEventListener(
   "click",
   function (event) {
+    // adds the ability to strikethrough no matter where you click on the list item
     if (
       event.target.tagName === "LI" ||
       event.target.classList.contains("item") ||
@@ -113,7 +129,8 @@ listContainer.addEventListener(
         li.classList.toggle("checked");
         saveData();
       }
-    } else if (event.target.tagName === "CLOSE") {
+      // remove item from list on click of close x
+    } else if (event.target.classList.contains("close")) {
       event.target.parentElement.remove();
       saveData();
     }
@@ -121,16 +138,20 @@ listContainer.addEventListener(
   false
 );
 
+// function to save the data to local storage
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
+// function to add items from localstorage to the list
 function showTaskList() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 
+// call fuction to render the list
 showTaskList();
 
+// add the ability to use the enter key to add items to the cart
 document.body.addEventListener("keydown", (event) => {
   if (
     event.key === "Enter" &&
